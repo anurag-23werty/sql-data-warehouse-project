7f533406add55c757fee65853f644a091d5c56ba
Anurag Choudhury <anurag.omm11@gmail.com>
2026-05-15 23:24:17 +0530

    Create quality_check_gold.sql

---
+++ test/quality_check_gold.sql
@@ -0,0 +1,56 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    Validates integrity, consistency, and accuracy
+    of the Gold Layer.
+===============================================================================
+*/
+
+
+-- ====================================================================
+-- Checking gold.dim_customers
+-- ====================================================================
+
+-- Uniqueness of customer_key
+SELECT
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+
+
+-- ====================================================================
+-- Checking gold.dim_products
+-- ====================================================================
+
+-- Uniqueness of product_key
+SELECT
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+
+
+-- ====================================================================
+-- Checking gold.fact_sales
+-- ====================================================================
+
+-- Referential integrity between fact and dimensions
+SELECT *
+
+FROM gold.fact_sales f
+
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+
+WHERE p.product_key IS NULL
+   OR c.customer_key IS NULL;
